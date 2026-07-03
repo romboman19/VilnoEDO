a4931b4b1c986cbd6730ad14d96edbfecad1cab3
Roman <romboman19@gmail.com>
2026-07-03 20:32:02 +0300

    feat: default UA KEP client to file-key method

--- packages/ua-kep/client/hooks/use-signing-method.ts
+++ packages/ua-kep/client/hooks/use-signing-method.ts
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import type { TUaKepSigningMethod } from '../../types/signing-methods';
 
-export const useSigningMethod = (initialMethod: TUaKepSigningMethod = 'privatbank-jks') => {
+export const useSigningMethod = (initialMethod: TUaKepSigningMethod = 'file-key') => {
   const [signingMethod, setSigningMethod] = useState<TUaKepSigningMethod>(initialMethod);
 
   return {
