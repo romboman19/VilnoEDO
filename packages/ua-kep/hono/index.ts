b0a5d51532ce593fe00664c3c4f74c6713e4e1bb
Roman <romboman19@gmail.com>
2026-07-03 23:44:21 +0300

    feat: expose UA KEP evidence manifest
    feat: expose UA KEP evidence manifest

--- packages/ua-kep/hono/index.ts
+++ packages/ua-kep/hono/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 
 import { bootstrapRoute } from './bootstrap';
 import { completeRoute } from './complete';
+import { evidenceRoute } from './evidence';
 import { pkiProxyRoute } from './pki-proxy';
 import { prepareRoute } from './prepare';
 
@@ -10,4 +11,5 @@ export const uaKep = new Hono();
 uaKep.route('/bootstrap', bootstrapRoute);
 uaKep.route('/prepare', prepareRoute);
 uaKep.route('/complete', completeRoute);
+uaKep.route('/evidence', evidenceRoute);
 uaKep.route('/pki', pkiProxyRoute);
